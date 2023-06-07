78df28bfb5a79f0d15d0b3d6267e35e5d39211c5
DESKTOP-8O171KB\takuz <takuzaburou.negi@gmail.com>
2023-06-08 02:06:49 +0900

    背景完成

--- src/Aspect_logistics_game/JFrame1.java
+++ src/Aspect_logistics_game/JFrame1.java
@@ -10,6 +10,7 @@ import javax.swing.JScrollPane;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
 
 import javax.swing.JTabbedPane;
 import javax.swing.BoxLayout;
@@ -130,11 +131,14 @@ class mapCanvas extends Canvas{
 	int h = 720;
 	
 	Graphics2D g2d;
-	Ellipse2D.Double[] ellipse;
 	BufferedImage cImage = null;
 	
 	BasicStroke basicStroke;
 	BasicStroke dottedStroke;
+	
+	Ellipse2D.Double[] ellipse;
+	Line2D.Double[] line2D;
+	
 	
 	public mapCanvas() {//初期化処理
 		cImage = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
@@ -144,19 +148,29 @@ class mapCanvas extends Canvas{
 		
 		//アンチエイリアス設定
 		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		
-		//背景画像設定
-		g2d.setBackground(Color.white);
-		ellipse = new Ellipse2D.Double[logistics_game.circleNum];
-		for(int i = 0;i < logistics_game.circleNum;i++) {
-			ellipse[i] = new Ellipse2D.Double(logistics_game.circleposx[i], logistics_game.circleposy[i], logistics_game.circleSize, logistics_game.circleSize);
-		}
+
 		
 		//通常線
 		basicStroke = new BasicStroke(3.0f);
 		
 		//破線
 		dottedStroke = new BasicStroke(3.0f,BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER,10.0f,new float[] {4.0f, 2.0f},0.0f); 
+	
+
+		//背景円描画設定
+		ellipse = new Ellipse2D.Double[logistics_game.circleNum];
+		for(int i = 0;i < logistics_game.circleNum;i++) {
+			ellipse[i] = new Ellipse2D.Double(logistics_game.circleposx[i], logistics_game.circleposy[i], logistics_game.circleSize, logistics_game.circleSize);
+		}
+		
+		//背景線描画設定
+		line2D = new Line2D.Double[logistics_game.passNum];
+		for(int i = 0;i<logistics_game.passNum;i++) {
+			line2D[i] = new Line2D.Double(  logistics_game.circleposx[logistics_game.node1[i]] + logistics_game.circleSize/2,
+											logistics_game.circleposy[logistics_game.node1[i]] + logistics_game.circleSize/2,
+											logistics_game.circleposx[logistics_game.node2[i]] + logistics_game.circleSize/2,
+											logistics_game.circleposy[logistics_game.node2[i]] + logistics_game.circleSize/2);
+		}
 	}
 	
 	@Override
@@ -167,8 +181,16 @@ class mapCanvas extends Canvas{
 		g2d.setStroke(basicStroke);
 		//g2d.setStroke(dottedStroke);
 		
-		for(Ellipse2D.Double i:ellipse) {
+		for(Ellipse2D.Double i:ellipse) {//円描画
 			g2d.draw(i);
+		}
+		
+		//線描画
+		for(int i = 0;i<logistics_game.passNum;i++) {
+			if (i+1 > logistics_game.passNum - logistics_game.highCostPassNum) {
+				g2d.setStroke(dottedStroke); //破線に切り替え
+			}
+			g2d.draw(line2D[i]);
 		}
 		
 		//バッファを描画
--- src/Aspect_logistics_game/logistics_game.java
+++ src/Aspect_logistics_game/logistics_game.java
@@ -11,9 +11,13 @@ public class logistics_game {
 	public static final int circleNum = 9;
 	public static final int circleSize = 100;
 	public static final int[] circleposx = {10, 200,400,500,650,700,800,950,1100}; // ノードの位置
-	public static final int[] circleposy = {600,550,200,400,600,0  ,300,550 ,500};
+	public static final int[] circleposy = {600,550,200,400,600,0  ,300,550 ,400};
 	
 	public static final int passNum = 10;//経路の経由地点とコスト
+	public static final int highCostPassNum = 2;
+	public static final int[] node1 = {0,1,3,3,3,6,4,7,2,6};
+	public static final int[] node2 = {1,3,2,4,6,5,7,8,5,8};
+	public static final int[] cost  = {1,1,1,1,1,1,1,1,5,5};
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
